46fae84ca362ebc97a0d2be13460a7de40c30a5b
Sean Murphy <sean@iamseanmurphy.com>
2014-09-01 15:21:31 -0400

    Phase 1 development mostly complete

--- www/js/app.js
+++ www/js/app.js
@@ -1,1 +1,20 @@
-App = Ember.Application.create({});
+App = Ember.Application.create();
+
+App.Router.map(function() {
+	this.resource('sight', { path: '/sights/:sight_id' });
+});
+
+App.IndexRoute = Ember.Route.extend({
+	model: function() {
+		return sights;
+	}
+});
+
+App.SightRoute = Ember.Route.extend({
+	model: function(params) {
+		return sights[params.sight_id - 1];
+	},
+	serialize: function(sight) {
+		return { sight_id: sight.get('id') };
+	}
+});
---
+++ www/js/data.js
@@ -0,0 +1,39 @@
+var sights = [
+	{
+		id: 1,
+		name: "Sight 1",
+		address: "123 Main St, Some City, ST 12345",
+		lat: 43.6799024,
+		lng: -70.2610889,
+		description: "This is a description.",
+		photos: [
+			"img/photo-1.jpg"
+		]
+	},
+	{
+		id: 2,
+		name: "Sight 2",
+		address: "123 Main St, Some City, ST 12345",
+		lat: 43.6799024,
+		lng: -70.2610889,
+		description: "This is a longer description of stuff.",
+		photos: [
+			"img/photo-2.jpg",
+			"img/photo-3.jpg",
+			"img/photo-4.jpg"
+		]
+	},
+	{
+		id: 3,
+		name: "Sight 3",
+		address: "123 Main St, Some City, ST 12345",
+		lat: 43.6799024,
+		lng: -70.2610889,
+		description: "This is a description of the place you could visit and have a great time you will remember for many days into the indefinite future and reminisce with fondness about the extraordinary experiences you've enjoyed in your life and the happiness it brings you when remembering that time you went that place and did that thing--this thing--and so you should do it and go there and experience the experiences that will last a lifetime.",
+		photos: [
+			"img/photo-3.jpg",
+			"img/photo-4.jpg",
+			"img/photo-5.jpg"
+		]
+	}
+];
